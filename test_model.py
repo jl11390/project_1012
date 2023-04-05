044d44dc6d97e7f7b1749acf914dda2a1d3038db
Junze Li <junzeli@junzes-air.mynetworksettings.com>
2023-04-05 14:26:11 -0400

    first train baseline

--- test_model.py
+++ test_model.py
@@ -43,13 +43,13 @@ if __name__ == "__main__":  # Use this script to test your model
     hate_speech = load_dataset("hate_speech_offensive")
     split = hate_speech["train"].train_test_split(.1, seed=3463)
     hate_speech["test"] = split["test"]
-    
+
     # Preprocess the dataset for the tester
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     hate_speech["test"] = preprocess_dataset(hate_speech["test"], tokenizer)
 
     # Set up tester
-    tester = init_tester("models/run-0/checkpoint-2000")
+    tester = init_tester("models/run-0/checkpoint-500")
 
     # Test
     results = tester.predict(hate_speech["test"])
